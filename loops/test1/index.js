5d80ad7143c402547c6b20c826ca29e7e6b840e4
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-03-31 14:16:27 +0300

    start js

---
+++ loops/test1/index.js
@@ -0,0 +1,9 @@
+const n = 120;
+let i = 1;
+let sum = 0;
+while (i<=n) {
+  
+  sum += i;
+  i++;
+}
+console.log('Result:' + sum);
---
+++ variables-and-data-types/test1/index.js
@@ -0,0 +1,7 @@
+const userName = 'John';
+const greeting1 = "I'm John";
+const hobby = 'Walking';
+const favouriteDessert = 'cake';
+const superNumber = 17;
+console.log(userName)
+
