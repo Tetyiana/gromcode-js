c795c3807ca868bea76ced5fe7bce64c33a77e9f
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-05-12 18:00:36 +0300

    fix jest

--- modules-and-unit-tests/first-tests-with-jest/index.test.js
+++ modules-and-unit-tests/first-tests-with-jest/index.test.js
@@ -2,13 +2,13 @@ const getEvenNumbers = arr => {
   arr.filter(num => num % 2 == 0);
 };
 it('17 equal 17', () => {
-  expect(17.toEqual(17));
+  expect(17).toEqual(17);
 });
 
 it('17 is not equal 18', () => {
-  expect(17.not.toEqual(18));
+  expect(17).not.toEqual(18);
 });
-it('hould get only even numbers from array', () => {
+it('should get only even numbers from array', () => {
   const result = getEvenNumbers([1, 2, 3, 4]);
   expect(result).toEqual([2, 4]);
 });
