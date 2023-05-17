6a1cc1a7f3ed477f6312bd664a9079b15d87e31a
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-05-17 19:39:36 +0300

    test-code

---
+++ modules-and-unit-tests/test-code/calculator.test.js
@@ -0,0 +1,14 @@
+import getSum { getOddNumbers, getSquaredArray } from "./calculator.js";
+
+it(' should get squared numbers', () => {
+  const result = getSquaredArray([1, 2, 3]);
+  expect(result).toEqual([1, 4, 9]);
+})
+it('should get odd numbers', () => {
+  const result = getOddNumbers([1, 2, 3, 4, 5]);
+  expect(result).toEqual([1, 3, 5]);
+})
+it('should get sum numbers', () => {
+  const result = getSum(7, 5);
+  expect(result).toEqual(12);
+})
