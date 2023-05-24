40e7eb49c806c0099bc99bcb5918a214a2eb8350
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-05-24 13:49:17 +0300

    fix default export

--- modules-and-unit-tests/test-code/calculator.js
+++ modules-and-unit-tests/test-code/calculator.js
@@ -1,3 +1,3 @@
 export const getSquaredArray = (arr) => arr.map((el) => el ** 2);
 export const getOddNumbers = (arr) => arr.filter((el) => el % 2 !== 0);
-export getSum = (a, b) => a + b;
+export default (a, b) => a + b;
