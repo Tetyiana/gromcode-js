d3b1ec54b8849cda2e36f8283c91c89b71c82585
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-05-24 13:06:04 +0300

    fix getSum

--- modules-and-unit-tests/test-code/calculator.js
+++ modules-and-unit-tests/test-code/calculator.js
@@ -1,3 +1,3 @@
 export const getSquaredArray = (arr) => arr.map((el) => el ** 2);
 export const getOddNumbers = (arr) => arr.filter((el) => el % 2 !== 0);
-const sum = (a, b) => a + b;
+const getSum = (a, b) => a + b;
