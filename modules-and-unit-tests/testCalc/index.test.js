1c423e5ee0313a11a1d748209b9a495742bfdad6
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-05-30 16:11:07 +0300

    fix toEqual

--- modules-and-unit-tests/testCalc/index.test.js
+++ modules-and-unit-tests/testCalc/index.test.js
@@ -2,22 +2,22 @@ import { calc } from "./calculator.js";
 
 it('перевірка типу аргумента ', () => {
  const result = calc();
-  expect(result).toBe(null);
+  expect(result).toEqual(null);
 });
 
 it('перевірка суми', () => {
   const result = calc ('3 + 3');
-  expect(result).toBe('3 + 3 = 6');
+  expect(result).toEqual('3 + 3 = 6');
 });
 it('перевірка різниці', () => {
   const result = calc ('5 - 2');
-  expect(result).toBe('5 - 2 = 3');
+  expect(result).toEqual('5 - 2 = 3');
 });
 it('перевірка множення', () => {
   const result = calc ('5 * 3');
-  expect(result).toBe('5 * 3 = 15');
+  expect(result).toEqual('5 * 3 = 15');
 });
 it('перевірка ділення', () => {
   const result = calc ('6 / 2');
-  expect(result).toBe('6 / 2 = 3');
+  expect(result).toEqual('6 / 2 = 3');
 });
