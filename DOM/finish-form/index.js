b93f00f5fd6c870eae7e7d07015f6da1e4309a81
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-06-28 17:33:25 +0300

    finish form

---
+++ DOM/finish-form/index.js
@@ -0,0 +1,12 @@
+export function finishForm() {
+  const finishFormElem = document.querySelector(".login-form");
+  const inputElemLogin = document.createElement("input");
+  inputElemLogin.type = "text";
+  inputElemLogin.name = "login"
+  finishFormElem.prepend(inputElemLogin);
+  const inputElemPassword = document.getElementsByTagName('password');
+  inputElemPassword.tape = 'password';
+}
+
+
+
