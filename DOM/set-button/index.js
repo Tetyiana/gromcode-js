8743081d418f1cfd8b698e47cf792d7e5dd608d9
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-06-28 16:08:43 +0300

    2

--- DOM/set-button/index.js
+++ DOM/set-button/index.js
@@ -1,6 +1,12 @@
+// export function setButton(buttonText) {
+//   const titleElem = document.querySelector("body");
+//   titleElem.createElement.innerHTML = "<button>buttonText</button>";
+
+// }
 export function setButton(buttonText) {
-  const titleElem = document.querySelector("body");
-  titleElem.createElement.innerHTML = "<button>buttonText</button>";
+  const bodyElem = document.querySelector("body");
+  const buttonElem = document.createElement("button");
+  buttonElem.innerHTML = `<button>buttonText</button>`;
 
 }
 
