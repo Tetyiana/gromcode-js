3f6bc2f5f9b115401b041e7c3df7e7c261dcb7d0
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-06-28 16:52:45 +0300

    1

--- DOM/create-element/index.js
+++ DOM/create-element/index.js
@@ -1,10 +1,21 @@
 export function finishList() {
   const finishListElem = document.querySelector(".list");
-  const liElem = document.createElement("li");
-  liElem.append('1');
-  liElem.prepend('8');
-  liElem.before.document.querySelector(".special")('4');
-  liElem.after.document.querySelector(".special")('6');
+  const liElem1 = document.createElement("li");
+  liElem1.textContent = '8';
+  finishListElem.append
+    (liElem1);
+  const liElem2 = document.createElement("li");
+  liElem2.textContent = '1';
+  finishListElem.prepend(liElem2);
+  const finishListElemSpecial = document.querySelector(".special");
+  const liElem3 = document.createElement("li");
+  liElem3.textContent = '4';
+  finishListElemSpecial.before(liElem3);
+  const liElem4 = document.createElement("li");
+  liElem4.textContent = '6';
+  finishListElemSpecial.after(liElem4);
 }
+
+
 
 
