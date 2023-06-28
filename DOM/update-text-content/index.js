4848c826e5c046e64aaf20f0067ce22f5fce195c
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-06-28 15:45:39 +0300

    2

--- DOM/update-text-content/index.js
+++ DOM/update-text-content/index.js
@@ -1,10 +1,5 @@
 export function setTitle(text) {
   const titleElem = document.querySelector('.title');
   titleElem.textContent = text;
-  // console.log(titleElem.textContent);
-
 }
-setTitle('i am a programme');
-
-
 
