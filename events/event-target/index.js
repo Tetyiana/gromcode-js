840b37deda6a966fe38d53c802c5ea77a32a50ff
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-06-30 19:56:44 +0300

    2

--- events/event-target/index.js
+++ events/event-target/index.js
@@ -1,8 +1,5 @@
 const buttonElem = document.querySelector('btn');
 const handleClick = (event) => {
   console.log(event.target.textContent);
-  buttonElem.addEventListener("click", handleClick);
-
-}
-
-
+};
+buttonElem.addEventListener("click", handleClick);
