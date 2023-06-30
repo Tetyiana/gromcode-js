f6d02ffe41d127a36358ff26f75e498b60882a07
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-06-30 14:31:44 +0300

    1

--- DOM/closest/index.js
+++ DOM/closest/index.js
@@ -1,11 +1,24 @@
+// function getSection(num) {
+//   const elemOfChild = document.querySelector('.box');
+//   if (elemOfChild) {
+//     const spanElement = elemOfChild.querySelector(`span[data-number='${num}']`);
+//     if (spanElement) {
+//       const parentElement = spanElement.parentElement;
+//       return parentElement.getAttribute('data-section');
+//     }
+//   }
+// }
+
 export function getSection(num) {
-  const elemOfChild = document.querySelector('.box');
-  if (elemOfChild) {
-    const spanElement = elemOfChild.querySelector(`span[data-number='${num}']`);
-    if (spanElement) {
-      const parentElement = spanElement.parentElement;
-      return parentElement.getAttribute('data-section');
-    }
+
+
+  const spanElement = document.querySelector(`span[data-number='${num}']`);
+  if (spanElement) {
+
+
+    const parentElement = spanElement.parentElement;
+
+
+    return parentElement.getAttribute('data-section');
   }
 }
-
