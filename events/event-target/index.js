4b17fea28439adc98027a02f09e4023d37f6d9af
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-06-30 20:11:16 +0300

    4

--- events/event-target/index.js
+++ events/event-target/index.js
@@ -1,19 +1,9 @@
-// const buttonElem = document.querySelector('btn');
-// const handleClick = (event) => {
-//   console.log(event.target.textContent);
-// };
-// buttonElem.addEventListener("click", handleClick);
-
 const elemButtonOne = document.querySelector('.btn:nth-of-type(1)');
 const elemButtontwo = document.querySelector('.btn:nth-of-type(2)');
 
 function handleClick(event) {
   console.log(event.target.textContent);
 }
-
-// function handleClickTwo(event) {
-//   console.log(event.target.textContent);
-// }
 
 elemButtonOne.addEventListener('click', handleClick);
 elemButtontwo.addEventListener('click', handleClick);
