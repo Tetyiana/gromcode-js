42e2c544b0418a941924611307e4cf75c07dd333
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-06-30 18:07:51 +0300

    1

--- events/single/index.js
+++ events/single/index.js
@@ -1,4 +1,8 @@
 const clickButton = document.querySelector('.single-use-btn');
-const outputText = () => { console.log('clicked') };
+
+const outputText = () => {
+  console.log('clicked');
+  clickButton.removeEventListener('click', outputText);
+};
+
 clickButton.addEventListener('click', outputText);
-clickButton.removeEventListener('click', outputText);
