ed66a4c4124519711091decda83eccdb6767283c
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-06-30 14:43:37 +0300

    1

---
+++ DOM/getItemList/index.js
@@ -0,0 +1,13 @@
+function getItemList() {
+  const elementsList = document.querySelectorAll('.technology');
+  console.dir(elementsList);
+  return elementsList;
+
+}
+function getItemArray() {
+  const elementsArray = document.querySelectorAll('.tool');
+  console.dir(elementsArray);
+
+  return elementsList;
+
+}
