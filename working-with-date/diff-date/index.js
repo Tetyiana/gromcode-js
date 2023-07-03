7351c364cc75697b6714a1588b8327151dc6b7b1
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-07-03 11:16:20 +0300

    getDiff

---
+++ working-with-date/diff-date/index.js
@@ -0,0 +1,13 @@
+export const getDiff = (startDate, endDate) => {
+
+  const diff = Math.abs(startDate.getTime() - endDate.getTime());
+
+  const days = Math.floor(diff / (24 * 60 * 60 * 1000));
+  const hours = Math.floor((diff % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+  const minutes = Math.floor((diff % (60 * 60 * 1000)) / (60 * 1000));
+  const seconds = Math.floor((diff % (60 * 1000)) / 1000);
+
+  return `${days}d, ${hours}h, ${minutes}m, ${seconds}s`;
+}
+
+
