62ff00dcf4930f50450e7e927c038881899f78fd
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-07-09 18:37:18 +0300

    2

--- functions-advanced-approaches/compose/index.js
+++ functions-advanced-approaches/compose/index.js
@@ -1,8 +1,10 @@
 const add2 = value => value + 2;
 const square = value => value * value;
 const half = value => value / 2;
+
 export const compose = (...funcs) => value => {
-  return
+  return funcs.reduce((result, func) => func(result), value);
 };
+
 const superFunc = compose(add2, square, half);
-// superFunc(4);
+
