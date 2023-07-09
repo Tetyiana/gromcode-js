342dab0a0c84001e1f2e38f8980a7fd3b1c8e2d2
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-07-09 18:03:32 +0300

    1

--- functions-advanced-approaches/mult/index.js
+++ functions-advanced-approaches/mult/index.js
@@ -1,3 +1,4 @@
-const mult = (a) => (b) => a * b;
-export const twice = mult * 2;
-export const triple = mult * 3;
+export const mult = (a) => (b) => a * b;
+export const twice = mult(2);
+export const triple = mult(3);
+
