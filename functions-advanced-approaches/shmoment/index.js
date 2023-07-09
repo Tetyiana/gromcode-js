67d52bc1a755c21a09a3f262318c6c79f42af289
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-07-09 19:10:16 +0300

    shmoment

---
+++ functions-advanced-approaches/shmoment/index.js
@@ -0,0 +1,44 @@
+export const shmoment = (date) => {
+  const resultDate = new Date(date);
+
+  const add = (value, num) => {
+    switch (value) {
+      case 'years':
+        resultDate.setFullYear(resultDate.getFullYear() + num);
+        break;
+      case 'months':
+        resultDate.setMonth(resultDate.getMonth() + num);
+        break;
+      case 'days':
+        resultDate.setDate(resultDate.getDate() + num);
+        break;
+      case 'hours':
+        resultDate.setHours(resultDate.getHours() + num);
+        break;
+      case 'minutes':
+        resultDate.setMinutes(resultDate.getMinutes() + num);
+        break;
+      case 'seconds':
+        resultDate.setSeconds(resultDate.getSeconds() + num);
+        break;
+      default:
+        break;
+    }
+    return shmoment(resultDate);
+  };
+
+  const subtract = (value, num) => {
+    return add(value, -num);
+  };
+
+  const result = () => {
+    return resultDate;
+  };
+
+  return {
+    add,
+    subtract,
+    result,
+  };
+};
+
