23fb735743c382c942986cb729f924ea5e7a03e1
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-07-09 15:46:05 +0300

    2

--- functions-advanced-approaches/calc/index.js
+++ functions-advanced-approaches/calc/index.js
@@ -1,28 +1,29 @@
 export const calc = (initValue) => {
   let result = initValue;
   const calculator = {
-    const add(number) {
+    add(number) {
       result += number;
-  return this;
-};
-const mult(number) {
-  result *= number;
-  return this;
-};
-const substract(number) {
-  result -= number;
-  return this;
-};
-const div(number) {
-  result /= number;
-  return this;
-};
-const result() {
-  return result;
-};
+      return this;
+    },
+    mult(number) {
+      result *= number;
+      return this;
+    },
+    subtract(number) {
+      result -= number;
+      return this;
+    },
+    div(number) {
+      result /= number;
+      return this;
+    },
+    result() {
+      return result;
+    },
   };
-return this
+  return this;
 };
+
 
 // const result = calc(1).mult(6).div(2).substract(5).result();
 // console.log(result);
