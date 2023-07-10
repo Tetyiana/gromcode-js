f85e5e1b9855d20d1af05094476d17c341f0872a
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-07-10 13:02:01 +0300

    try12

--- asynchronous-code/load-image/index.js
+++ asynchronous-code/load-image/index.js
@@ -11,9 +11,7 @@ export const addImage = (imgSrc, callback) => {
 
   image.addEventListener('error', () => callback(new Error('Image load is failed')));
 };
-const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
 
-// callack function
 const onImageLoaded = (error, imgElem) => {
   if (error) {
     console.log(error);
@@ -26,14 +24,8 @@ const onImageLoaded = (error, imgElem) => {
   sizeElem.textContent = `${width} x ${height}`;
 };
 
-
+// examples
 addImage(
   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
   onImageLoaded,
 );
-
-// examples
-// addImage(
-//   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-//   onImageLoaded,
-// );
