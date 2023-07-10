fcab5406804a719bb0089cd33ad8d5aa5becd104
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-07-10 17:01:44 +0300

    try10

--- promises-intro/promisification/index.js
+++ promises-intro/promisification/index.js
@@ -1,23 +1,4 @@
-const addImage = (url, callback) => {
-  const img = document.createElement('img');
-  img.setAttribute('alt', 'User avatar');
-  img.src = url;
-
-  const pageElem = document.querySelector('.page');
-  pageElem.append(img);
-
-  const onImageLoaded = () => {
-    const { width, height } = img;
-    callback(null, { width, height });
-  };
-
-  const onImageLoadError = () => callback('Image load failed');
-
-  img.addEventListener('load', onImageLoaded);
-
-  img.addEventListener('error', onImageLoadError);
-};
-
+import { addImage } from "./addImage";
 export const addImageV2 = url => {
   return new Promise((resolve, reject) => {
     addImage(url, (error, data) => {
@@ -33,6 +14,6 @@ export const addImageV2 = url => {
 
 // examples
 
-// addImageV2('https://server.com/image.png')
-//   .then(data => console.log(data)) // ==> { width: 200, height: 100 }
-//   .catch(error => console.log(error)); // ==> 'Image load failed'
+addImageV2('https://server.com/image.png')
+  .then(data => console.log(data)) // ==> { width: 200, height: 100 }
+  .catch(error => console.log(error)); // ==> 'Image load failed'
