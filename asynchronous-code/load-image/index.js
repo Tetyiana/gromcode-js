46bf7f63d80513326604341674da2df8df9c861b
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-07-10 12:48:05 +0300

    try 9

--- asynchronous-code/load-image/index.js
+++ asynchronous-code/load-image/index.js
@@ -1,18 +1,15 @@
 export const addImage = (imgSrc, callback) => {
-  const imgElem = document.createElement('img');
-  imgElem.setAttribute('alt', 'My Photo');
-  imgElem.src = imgSrc;
-  const containerElem = document.querySelector('.page');
-  containerElem.append(imgElem);
+  const page = document.querySelector('.page');
+  const image = document.createElement('img');
+  image.setAttribute('alt', 'Blue Lakes');
+  image.src = imgSrc;
+  page.append(image);
 
-  const onImageLoaded = () => {
-    const { width, height } = imgElem;
-    callback(null, { width, height });
-  };
+  image.addEventListener('load', () => {
+    callback(null, image);
+  });
 
-  imgElem.addEventListener('load', onImageLoaded);
-  imgElem.addEventListener('error', () => callback('Image load failed'));
-
+  image.addEventListener('error', () => callback(new Error('Image load is failed')));
 };
 const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
 
