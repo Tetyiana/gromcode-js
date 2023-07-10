0ee2cb72c1b1a853c6ed6c188b49c4c60b3f6d69
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-07-10 17:40:34 +0300

    promise

---
+++ promises-intro/обработчики-успешного-промиса/index.js
@@ -0,0 +1,32 @@
+/* ===> 1 <=== */
+
+/*
+ * successPromise должен зарезолвить число 67
+ * Ответьте себе на вопрос, какой тип данных имеет переменная successPromise
+ */
+
+
+const successPromise = new Promise(resolve => {
+  resolve(67);
+});
+
+/*
+ * допишите обработчик успешного промиса (аргументы и тело ф-ции onSuccess)
+ * чтобы в консоль вывелся квадрат числа, которое резолвиться из successPromise
+ */
+
+
+successPromise.then(function onSuccess(number) {
+  const square = number ** 2;
+  console.log(square);
+});
+
+/*
+ * в обработчике ошибок промиса (ф-ция onError внутри .catch()) выведите в консоль текст 'I am an error'
+ * текст вывелся в консоль?
+ * подумайте почему
+ */
+
+successPromise.catch(function onError() {
+  console.log('I am an error');
+});
