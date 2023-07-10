984c376911bbfe0b5dd3bb360a54a9fa7b9b64ae
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-07-10 16:23:00 +0300

    try 1

---
+++ promises-intro/promisification/index.js
@@ -0,0 +1,17 @@
+export const addImageV2 = url => {
+  return new Promise((resolve, reject) => {
+    addImage(url, (error, data) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+// examples
+
+addImageV2('https://server.com/image.png')
+  .then(data => console.log(data)) // ==> { width: 200, height: 100 }
+  .catch(error => console.log(error)); // ==> 'Image load failed'
