804344b981e1a9ac5315bf2342fead58b69980fc
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-07-11 13:00:37 +0300

    requestUserData

---
+++ promise-handlers/promise-handlers/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @param {string} userId
+ * @return {promise}
+ */
+export const requestUserData = userId => {
+  // put your code here
+  const request = new Promise((resolv, reject) => {
+    if (userId === 'broken') {
+      setTimeout(() => {
+        reject(new error('User not found'));
+      }, 500);
+    } else {
+      setTimeout(() => {
+        resolv({
+          name: 'John',
+          age: 17,
+          userId: `${userId}`,
+          email: `${userId}@example.com`,
+        });
+      }, 1000);
+    }
+  });
+
+  return request;
+
+};
