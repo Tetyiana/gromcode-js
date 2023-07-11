d3ca9856cafc5ba77f928ee61657ff5fb0dc47df
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-07-11 22:22:46 +0300

    try1

--- promise-handlers/fix-chain-promises-2/index.js
+++ promise-handlers/fix-chain-promises-2/index.js
@@ -9,18 +9,21 @@ const serverResponsePromise = new Promise(resolve => {
     },
   };
   resolve(serverResponse);
-})
+});
 
-  /*
-   * допиши первый обработчик, чтобы во второй попал объект пользователя
-   */
+/*
+ * допиши первый обработчик, чтобы во второй попал объект пользователя
+ */
+
+serverResponsePromise
   .then(response => {
-    /* ...code here */
-
+    return response.json(); // Получаем данные из ответа сервера
   })
   .then(result => {
     console.log(result); // { name: 'John', age: 20 }
-  });
+
+    return result; // Возвращаем результат для передачи в следующий обработчик
+  })
 
 console.log(
   '!!! Обрати внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!',
