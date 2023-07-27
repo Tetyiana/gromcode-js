b00330bc316bf87b99e1df799113b58f578eb4e4
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-07-27 16:31:25 +0300

    try5

--- practice-with-http/create-user-form/index.js
+++ practice-with-http/create-user-form/index.js
@@ -1,32 +1,35 @@
-const baseUrl = "https://64b5368bf3dbab5a95c6f173.mockapi.io/api/v1/register";
 
-const registerAccount = () => {
-  const email = document.querySelector("#email").value;
-  const password = document.querySelector("#password").value;
-  const name = document.querySelector("#name").value;
-
-  const userData = { email, password, name };
-
-  return fetch(baseUrl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(userData),
-  }).then((response) => response.json());
-};
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector(".login-form");
+  const buttonEnable = document.querySelector(".submit-button");
 
   form.addEventListener("submit", (event) => {
     event.preventDefault();
+    const baseUrl = "https://64b5368bf3dbab5a95c6f173.mockapi.io/api/v1/register";
 
+    const registerAccount = () => {
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+      const name = document.getElementById("name").value;
+
+      const userData = { email, password, name };
+      console.log(userData);
+      return fetch(baseUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(userData),
+      });
+    };
     const isValid = form.reportValidity();
-    const buttonEnable = document.querySelector(".submit-button");
+
     if (!isValid) {
       buttonEnable.disabled = true;
+      return;
     }
+
     registerAccount().then((responseData) => {
       buttonEnable.disabled = false;
       form.reset();
@@ -34,3 +37,4 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
