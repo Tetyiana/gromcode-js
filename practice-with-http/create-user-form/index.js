e3428271d99a39ea5a54c9a083667d7b47ee0eea
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-09-17 20:39:02 +0300

    try6

--- practice-with-http/create-user-form/index.js
+++ practice-with-http/create-user-form/index.js
@@ -1,39 +1,53 @@
 const baseUrl = "https://64b5368bf3dbab5a95c6f173.mockapi.io/api/v1/register";
-document.addEventListener("DOMContentLoaded", function (event) {
+const form = document.querySelector(".login-form");
+const submitButton = document.querySelector(".submit-button");
+
+form.addEventListener("submit", function (event) {
   event.preventDefault();
-  const form = document.querySelector(".login-form");
-  const submitButton = document.querySelector(".submit-button");
 
-  form.addEventListener("submit", function () {
-    if (form.reportValidity()) {
-      submitButton.setAttribute("disabled", false);
-    }
+  if (form.reportValidity() === true) {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+    return;
+  }
 
-    const email = document.getElementById("email").value;
-    const name = document.getElementsByName("name").value;
-    const password = document.getElementsByName("password").value;
-    const body = { email, name, password };
+  const email = document.getElementById("email").value;
+  const name = document.getElementsByName("name").value;
+  const password = document.getElementsByName("password").value;
+  const body = { email, name, password };
 
-    function register(method, url, body = null) {
-      return new Promise(() => {
-        fetch(url, {
-          method,
-          body: JSON.stringify(body),
-          headers: { "Content-Type": "application/json;charset=utf-8" },
+  function register(method, url, body = null) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method,
+        body: JSON.stringify(body),
+        headers: { "Content-Type": "application/json;charset=utf-8" },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json();
         })
-          .then((response) => {
-            return response.json();
-          })
-      });
-    }
+        .then((data) => {
+          resolve(data);
+        })
+        .catch((error) => {
+          reject(error);
+        })
+        .finally(() => {
+          submitButton.disabled = false;
+        });
+    });
+  }
 
-    register("POST", baseUrl, body)
-      .then((response) => {
-        alert(JSON.stringify(response));
-        form.reset();
-        submitButton.setAttribute("disabled", 'true');
-      });
-  });
+  register("POST", baseUrl, body)
+    .then((response) => {
+      alert(JSON.stringify(response));
+      form.reset();
+    });
 });
+
 
 
