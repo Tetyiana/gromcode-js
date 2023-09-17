13167ba0dd432865943634767bd7caf06f05357f
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-09-17 17:37:36 +0300

    try4

--- practice-with-http/create-user-form/index.js
+++ practice-with-http/create-user-form/index.js
@@ -1,27 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
+const baseUrl = "https://64b5368bf3dbab5a95c6f173.mockapi.io/api/v1/register";
+document.addEventListener("DOMContentLoaded", function (event) {
+  event.preventDefault();
   const form = document.querySelector(".login-form");
   const submitButton = document.querySelector(".submit-button");
-
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    if (!form.reportValidity()) {
-      return;
-    }
-
-    const email = document.getElementById("email").value;
-    const name = document.getElementById("name").value;
-    const password = document.getElementById("password").value;
-    const body = { email, name, password };
-
-    const baseUrl = "https://64b5368bf3dbab5a95c6f173.mockapi.io/api/v1/register";
-
-    register("POST", baseUrl, body)
-      .then((response) => {
-        alert(JSON.stringify(response));
-        form.reset();
-      });
-  });
 
   form.addEventListener("input", function () {
     if (form.checkValidity()) {
@@ -31,11 +12,28 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  function register(method, url, body = null) {
-    return fetch(url, {
-      method: method,
-      body: JSON.stringify(body),
-      headers: { "Content-Type": "application/json;charset=utf-8" },
-    }).then((response) => response.json());
-  }
+  form.addEventListener("submit", function () {
+    if (!form.reportValidity()) {
+      return;
+    }
+    const email = document.getElementById("email").value;
+    const name = document.getElementById("name").value;
+    const password = document.getElementById("password").value;
+    const body = { email, name, password };
+
+    function register(method, url, body = null) {
+      return fetch(url, {
+        method,
+        body: JSON.stringify(body),
+        headers: { "Content-Type": "application/json;charset=utf-8" },
+      }).then((response) => {
+        return response.json();
+      });
+    }
+
+    register("POST", baseUrl, body).then((response) => {
+      alert(JSON.stringify(response));
+      form.reset();
+    });
+  });
 });
