2710b93c2d1b25984f9c2ad7d93489a7f33b6df5
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-09-19 22:06:33 +0300

    try3

--- promise-advanced-async-await/get-user-async-await/index.js
+++ promise-advanced-async-await/get-user-async-await/index.js
@@ -6,7 +6,7 @@ export const fetchUser = async (userId) => {
     }
     const userData = await response.json();
     return userData;
-  } catch (err) {
+  } catch (error) {
     throw new Error('Failed to fetch user');
   }
 };
