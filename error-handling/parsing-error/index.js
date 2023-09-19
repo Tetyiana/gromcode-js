0c9b34f48305e11af601eb5a9e39f04ce2257e16
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-09-19 16:50:34 +0300

    try2

--- error-handling/parsing-error/index.js
+++ error-handling/parsing-error/index.js
@@ -1,11 +1,39 @@
+// window.addEventListener('error', function onUnhandledError(err) {
+//   console.error('error', err.message);
+// });
+
+// let userParsingResult;
+
+// try {
+//   const user = JSON.parse(userParsingResult);
+//   console.log('User data: ', user);
+//   userParsingResult = 'success';
+// } catch (e) {
+//   userParsingResult = 'error';
+// } finally {
+//   console.log(`User parsing finished with ${userParsingResult}`);
+// }
+
+// let productParsingResult;
+
+// try {
+//   const product = JSON.parse(productParsing);
+//   console.log('Product data: ', product);
+//   productParsingResult = 'success';
+// } catch (e) {
+//   productParsingResult = 'error';
+// } finally {
+//   console.log(`Product parsing finished with ${productParsingResult}`);
+// }
 window.addEventListener('error', function onUnhandledError(err) {
   console.error('error', err.message);
 });
 
 let userParsingResult;
+let userJson = '{"id": 1, "name": "John", "email": "john@example.com"}'; // Валидный JSON
 
 try {
-  const user = JSON.parse(userParsingResult);
+  const user = JSON.parse(userJson);
   console.log('User data: ', user);
   userParsingResult = 'success';
 } catch (e) {
@@ -15,9 +43,10 @@ try {
 }
 
 let productParsingResult;
+let productJson = '{"id": 2, "name": "Product", "price": "invalid"}'; // Сломанный JSON
 
 try {
-  const product = JSON.parse(productParsing);
+  const product = JSON.parse(productJson);
   console.log('Product data: ', product);
   productParsingResult = 'success';
 } catch (e) {
@@ -25,3 +54,4 @@ try {
 } finally {
   console.log(`Product parsing finished with ${productParsingResult}`);
 }
+
