04ddde3f4dc5242c8abcf8f4fa29223ba38092d0
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-09-19 17:35:35 +0300

    try8

--- error-handling/errors-in-promise/index.js
+++ error-handling/errors-in-promise/index.js
@@ -5,21 +5,21 @@ successRequest
     throw new Error("Error with data");
   })
   .catch(function onError1(error) {
-    console.error('onError1', error.message); // Выводим ошибку 'Error with data' (Тест 1)
+    console.error('onError1', error.message);
   });
 
 const failRequest = Promise.reject(new Error('Something went wrong'));
 
 failRequest
   .catch(function onError2(error) {
-    console.error('onError2', error.message); // Выводим ошибку 'Something went wrong'
-    return Promise.reject(new Error('Server error')); // Возвращаем новый rejected промис (Тест 2)
+    console.error('onError2', error.message);
+    return Promise.reject(new Error('Server error'));
   })
   .then(function onSuccess2(data) {
-    console.log('onSuccess2', data); // Не должно выполниться (Тест 3)
+    console.log('onSuccess2', data);
   })
   .catch(function onError3(error) {
-    console.error('onError3', error.message); // Выводим ошибку 'Server error' (Тест 2)
+    console.error('onError3', error.message);
   });
 
 
