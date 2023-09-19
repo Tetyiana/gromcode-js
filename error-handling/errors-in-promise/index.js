74337728ecb2ff159a784852c6cba237062a033c
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-09-19 17:34:46 +0300

    try7

--- error-handling/errors-in-promise/index.js
+++ error-handling/errors-in-promise/index.js
@@ -13,7 +13,7 @@ const failRequest = Promise.reject(new Error('Something went wrong'));
 failRequest
   .catch(function onError2(error) {
     console.error('onError2', error.message); // Выводим ошибку 'Something went wrong'
-    throw err('Server error'); // Возвращаем новый rejected промис (Тест 2)
+    return Promise.reject(new Error('Server error')); // Возвращаем новый rejected промис (Тест 2)
   })
   .then(function onSuccess2(data) {
     console.log('onSuccess2', data); // Не должно выполниться (Тест 3)
@@ -24,29 +24,4 @@ failRequest
 
 
 
-// const successRequest = Promise.resolve({ name: 'Tom' });
-
-// successRequest
-//   .then(function onSuccess1(data) {
-//     // (1) Код, который вы хотите выполнить при успешном разрешении промиса
-//     console.log('Custom code in onSuccess1'); // Пример кода
-//   })
-//   .catch(function onError1(error) {
-//     console.error('onError1', error.message);
-//   });
-
-// const failRequest = Promise.reject(new Error('Something went wrong'));
-
-// failRequest
-//   .catch(function onError2(error) {
-//     console.error('onError2', error.message);
-//     // (2) Код, который вы хотите выполнить при ошибке
-//     console.log('Custom code in onError2'); // Пример кода
-//   })
-//   .then(function onSuccess2(data) {
-//     console.log('onSuccess2', data);
-//   })
-//   .catch(function onError3(error) {
-//     console.error('onError3', error.message);
-//   });
 
