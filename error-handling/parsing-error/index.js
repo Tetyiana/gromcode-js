e7e8b84ba5c6fd2cdb0676d32b2883c6da594943
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-09-19 16:57:42 +0300

    try3

--- error-handling/parsing-error/index.js
+++ error-handling/parsing-error/index.js
@@ -1,30 +1,3 @@
-// window.addEventListener('error', function onUnhandledError(err) {
-//   console.error('error', err.message);
-// });
-
-// let userParsingResult;
-
-// try {
-//   const user = JSON.parse(userParsingResult);
-//   console.log('User data: ', user);
-//   userParsingResult = 'success';
-// } catch (e) {
-//   userParsingResult = 'error';
-// } finally {
-//   console.log(`User parsing finished with ${userParsingResult}`);
-// }
-
-// let productParsingResult;
-
-// try {
-//   const product = JSON.parse(productParsing);
-//   console.log('Product data: ', product);
-//   productParsingResult = 'success';
-// } catch (e) {
-//   productParsingResult = 'error';
-// } finally {
-//   console.log(`Product parsing finished with ${productParsingResult}`);
-// }
 window.addEventListener('error', function onUnhandledError(err) {
   console.error('error', err.message);
 });
@@ -43,10 +16,10 @@ try {
 }
 
 let productParsingResult;
-let productJson = '{"id": 2, "name": "Product", "price": "invalid"}'; // Сломанный JSON
+
 
 try {
-  const product = JSON.parse(productJson);
+  const product = JSON.parse();
   console.log('Product data: ', product);
   productParsingResult = 'success';
 } catch (e) {
