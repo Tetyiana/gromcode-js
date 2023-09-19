a32263f10dd86ec74d5ee272f4a8b09734b8f66f
Tetiana Artibiakina <artibyakinatanya@gmail.com>
2023-09-19 17:17:22 +0300

    try4

--- error-handling/errors-in-promise/index.js
+++ error-handling/errors-in-promise/index.js
@@ -2,7 +2,7 @@ const successRequest = Promise.resolve({ name: 'Tom' });
 
 successRequest
   .then(function onSuccess1(data) {
-    throw new Error("Error whis data")
+    throw new Error("Error with data");
   })
   .catch(function onError1(error) {
     console.error('onError1', error.message);
@@ -12,16 +12,13 @@ const failRequest = Promise.reject(new Error('Something went wrong'));
 
 failRequest
   .catch(function onError2(error) {
-    debugger;
     console.error('onError2', error.message);
     return Promise.reject('Server error');
-
-
   })
   .then(function onSuccess2(data) {
     console.log('onSuccess2', data);
   })
   .catch(function onError3(error) {
     console.error('onError3', error.message);
-
   });
+
